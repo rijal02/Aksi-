77aa9357c95520a2605c00c51015777f7f82446a
D-24 <D-24@D-24>
2024-06-03 20:22:27 +0700

    aksi

---
+++ aksi_latihan/src/main/java/com/topliderrizal/aksi_latihan/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.topliderrizal.aksi_latihan;
+
+/**
+ *
+ * @author D-24
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("SIIUUUUU");
+    }
+}
